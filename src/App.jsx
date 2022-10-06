a1a1a8013b0daf9f0e5826b9282b1a8c7cc41d11
JulesAD96 <adonsijules9@gmail.com>
2022-10-06 21:56:47 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Route , Routes, BrowserRouter} from "react-router-dom"
+
+import Index from "./home/Index"
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App
